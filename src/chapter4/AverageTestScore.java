b19c687ecb4191c8099a40ec551d66621e86d7da
sageMode <nathanielmvana@gmail.com>
2024-05-07 15:07:03 +0200

    Added a Student AverageTestScore program

---
+++ src/chapter4/AverageTestScore.java
@@ -0,0 +1,29 @@
+package chapter4;
+
+import java.util.Scanner;
+
+//Find the average of each student's test scores
+public class AverageTestScore {
+
+    public static void main(String[] args){
+        //Initialize what we know
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Process all students
+        for(int i=0;i < numberOfStudents;i++){
+            double total =0;
+            for(int j =0; j < numberOfTests;j++){
+                System.out.println("Enter the score for test #" + (j+1));
+                double score = scanner.nextDouble();
+                total += score;
+            }
+            double average = total / numberOfTests;
+
+            System.out.println("The test average of students #" + (i + 1) + " is "+ average);
+        }
+    }
+
+}
