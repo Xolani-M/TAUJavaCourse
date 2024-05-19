36aabad55eb1857a3aee67e3a3db5eaf9bde1b8a
sageMode <nathanielmvana@gmail.com>
2024-05-19 04:11:30 +0200

    Added a search method to make random nums unique

--- src/chapter7/LotteryTicket.java
+++ src/chapter7/LotteryTicket.java
@@ -1,6 +1,8 @@
 package chapter7;
 
+import java.util.Arrays;
 import java.util.Random;
+
 
 public class LotteryTicket {
 
@@ -8,6 +10,7 @@ public class LotteryTicket {
     private static final int MAX_TICKET_NUMBER = 69;
     public static void main(String[] args){
         int[] ticket = generateNumbers();
+        Arrays.sort(ticket);
         printTicket(ticket);
     }
 
@@ -17,9 +20,29 @@ public class LotteryTicket {
         Random random = new Random();
 
         for(int i =0; i < LENGTH; i++){
-            ticket[i] = random.nextInt(1,69);
+            int randomNumber;
+
+            do{
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER);
+            }while(search(ticket,randomNumber));
+            ticket[i] = randomNumber;
         }
         return ticket;
+    }
+
+    /**
+     * Does a sequential search on the arr and find a value
+     * @param array array to search through
+     * @param numToSearchFor Value to search for
+     * @return true if found snd false if not
+     */
+    public static boolean search(int[] array, int numToSearchFor){
+        for(int value: array){
+            if(value == numToSearchFor){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void printTicket(int[] ticket){
