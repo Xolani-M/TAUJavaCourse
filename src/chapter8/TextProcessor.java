ee233b0b97e779e1e564af01cfd5e85eac2faac4
sageMode <nathanielmvana@gmail.com>
2024-05-20 01:06:27 +0200

    Added a addSpace method that adds spaces after each uppercase letter

--- src/chapter8/TextProcessor.java
+++ src/chapter8/TextProcessor.java
@@ -4,9 +4,11 @@ public class TextProcessor {
 
     public static void main(String[] args){
 
-        wordCount("My birthday month is September");
+//        wordCount("My birthday month is September");
 
-        reverseString("Camel");
+//        reverseString("Camel");
+
+        addSpaces("HelloWorld!IAmFromPlanetOz");
 
     }
 
@@ -38,6 +40,24 @@ public class TextProcessor {
             reversedString += text.charAt(i);
         }
         System.out.println("Reversed string: " + reversedString);
+    }
+
+    /**
+     * Adds spaces before each uppercase letter
+     * @param text jumbled text
+     */
+    public static void addSpaces(String text){
+        var modifiedText = new StringBuilder(text);
+
+        for (int i= 0; i <modifiedText.length(); i++){
+            if(i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+
+
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
     }
 
 }
